2a53bb789d61bca3512d099fb731ccfdc3358652
Harish yadav <harishdy@gmail.com>
2021-12-26 13:18:37 +0530

    setup the UI

---
+++ Auto_UILevel_UpDemo1/View/UI_Design.swift
@@ -0,0 +1,8 @@
+//
+//  UI_Design.swift
+//  Auto_UILevel_UpDemo1
+//
+//  Created by Harish on 22/12/21.
+//
+
+import Foundation
--- Auto_UILevel_UpDemo1/ViewController.swift
+++ Auto_UILevel_UpDemo1/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupView()
     }
 
-
+    func setupView(){
+        let nameLabel = makeLabel(withText:"Name")
+        let textField = makeTextField(withPlaceHolderText: "Enter Name")
+        view.addSubview(nameLabel)
+        view.addSubview(textField)
+        nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+        
+        textField.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 8).isActive = true
+        textField.firstBaselineAnchor.constraint(equalTo: nameLabel.firstBaselineAnchor).isActive = true
+        textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        nameLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+        
+    }
+    func makeLabel(withText text:String)->UILabel{
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        
+        return label
+    }
+    func makeTextField(withPlaceHolderText text:String)-> UITextField{
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = text
+        textField.backgroundColor = .lightGray
+        
+        return textField
+    }
 }
 
