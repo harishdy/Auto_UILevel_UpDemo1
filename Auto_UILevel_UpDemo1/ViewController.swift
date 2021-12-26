74fe36613951d50806cef5256b7ccd41e835e140
Harish yadav <harishdy@gmail.com>
2021-12-26 13:39:01 +0530

    Label Content Hugging & compression Resistance

--- Auto_UILevel_UpDemo1/ViewController.swift
+++ Auto_UILevel_UpDemo1/ViewController.swift
@@ -19,19 +19,23 @@ class ViewController: UIViewController {
         let textField = makeTextField(withPlaceHolderText: "Enter Name")
         view.addSubview(nameLabel)
         view.addSubview(textField)
+/*          ****label Content Hugging & compression Resistance****
+ 
         nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
         
         textField.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 8).isActive = true
         textField.firstBaselineAnchor.constraint(equalTo: nameLabel.firstBaselineAnchor).isActive = true
         textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
-        nameLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+        nameLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)*/
         
     }
     func makeLabel(withText text:String)->UILabel{
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = text
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 32)
         label.backgroundColor = .yellow
         
         return label
@@ -43,6 +47,22 @@ class ViewController: UIViewController {
         textField.backgroundColor = .lightGray
         
         return textField
+    }
+    public func makeImageView(named: String)-> UIImageView{
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: named)
+        return view
+    }
+    
+    public func makeButon(withText text:String)->UIButton{
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .blue
+        button.contentEdgeInsets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        return button
     }
 }
 
